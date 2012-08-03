b63b07111db004258ec3a798500182134735341d
Jay Bazuzi <jay@bazuzi.com>
2012-08-03 16:04:47 -0700

    Command output to the ConsoleView.
    If you open PashGui and run Get-ChildItem, it works. Running another command gives an error.
    
    - Implement Host.UserInterface (at least, enough to get things to display output)
    
    - Move hooking up the ConsoleView.ConsoleInput event to regular code. Some bug in the code generator was causing this to have an extra '<>' before.
    
    - Reorder MainWindow.ctor so the ConsoleView is set before it's used.
    
    - Add references from PashGui to the Pash projects.
    
    - Remove formatting policies from PashGui.csproj, as these should be inherited from the parent.

--- Source/PashGui/MainWindow.cs
+++ Source/PashGui/MainWindow.cs
@@ -2,49 +2,54 @@ using System;
 using Gtk;
 using System.Management.Automation.Runspaces;
 using System.Management.Automation.Host;
+using PashGui;
 
 public partial class MainWindow: Gtk.Window
-{	
-	readonly private Runspace runspace;
-	readonly private Host host;
-	private Pipeline currentPipeline;
-	public MainWindow (): base (Gtk.WindowType.Toplevel)
-	{
-		this.host = new Host ();
-		this.runspace = RunspaceFactory.CreateRunspace (this.host);
-		this.runspace.Open ();
-		Build ();
-	}
-	
-	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
-	{
-		Application.Quit ();
-		a.RetVal = true;
-	}
+{
+    readonly private Runspace runspace;
+    readonly private Host host;
+    private Pipeline currentPipeline;
 
-	void Execute (string command)
-	{
-		using (currentPipeline = this.runspace.CreatePipeline())
-		{
-			currentPipeline.Commands.Add (command);
-		
-			// Now add the default outputter to the end of the pipe and indicate
-			// that it should handle both output and errors from the previous
-			// commands. This will result in the output being written using the PSHost
-			// and PSHostUserInterface classes instead of returning objects to the hosting
-			// application.
-			currentPipeline.Commands.Add ("out-default");
-			currentPipeline.Commands [0].MergeMyResults (PipelineResultTypes.Error, PipelineResultTypes.Output);
-		
-			currentPipeline.Invoke ();
-		}
-	}
+    public MainWindow(): base (Gtk.WindowType.Toplevel)
+    {
+        Build();
+        this.consoleview1.ConsoleInput += this.OnConsoleview1ConsoleInput;
 
-	protected void OnConsoleview1ConsoleInput (object sender, MonoDevelop.Components.ConsoleInputEventArgs e)
-	{
-		string command = e.Text;
-		
-		Execute (command);
-	}
-	
+        this.host = new Host(this.consoleview1);
+        this.runspace = RunspaceFactory.CreateRunspace(this.host);
+        this.runspace.Open();
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    void Execute(string command)
+    {
+        using (currentPipeline = this.runspace.CreatePipeline())
+        {
+            currentPipeline.Commands.Add(command);
+
+            // Now add the default outputter to the end of the pipe and indicate
+            // that it should handle both output and errors from the previous
+            // commands. This will result in the output being written using the PSHost
+            // and PSHostUserInterface classes instead of returning objects to the hosting
+            // application.
+            currentPipeline.Commands.Add("out-default");
+            currentPipeline.Commands [0].MergeMyResults(PipelineResultTypes.Error, PipelineResultTypes.Output);
+
+            currentPipeline.Invoke();
+        }
+    }
+
+    protected void OnConsoleview1ConsoleInput(object sender, MonoDevelop.Components.ConsoleInputEventArgs e)
+    {
+        string command = e.Text;
+
+        Execute(command);
+    }
+
 }
