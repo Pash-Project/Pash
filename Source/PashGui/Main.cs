923313be45c5ae95cdce4f54fea682f427a48f82
Jay Bazuzi <jay@bazuzi.com>
2012-08-06 23:28:35 -0700

    Apply formatting options
    I hit all files in PashGui, and only these 2 changed.

--- Source/PashGui/Main.cs
+++ Source/PashGui/Main.cs
@@ -3,14 +3,14 @@ using Gtk;
 
 namespace PashGui
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
 }
