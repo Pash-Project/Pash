591d83d5993450c29b5041e608fff31a63dcac46
Jay Bazuzi <jay@bazuzi.com>
2012-08-11 15:31:17 -0700

    Script to regenerate parser

---
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/generate-parser.cmd
@@ -0,0 +1,14 @@
+@IF NOT DEFINED _ECHO ECHO OFF
+:::::::::::::::::::::::
+:: Consuming the parser grammar, generate the compiled tables, the skeleton program, and the test 
+:::::::::::::::::::::::
+
+set PATH=%PATH%;..\..\..\..\GoldBuilder
+
+GOLDBuild.exe -verbose PashGrammar.grm 
+IF ERRORLEVEL 1 GOTO :EOF
+
+GOLDProg.exe PashGrammar.cgt ParserTemplate.pgt PashParser.Generated.cs 
+IF ERRORLEVEL 1 GOTO :EOF
+
+
