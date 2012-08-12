bc08cfe043b2f06e37f9d77bc3960fccbbbed151
Jay Bazuzi <jay@bazuzi.com>
2012-08-12 10:42:40 -0700

    Error checking when running GOLDBuild
    It doesn't set errorlevel, so we have to check for it a different way.

--- Source/Pash.System.Management/Pash/ParserIntrinsics/generate-parser.cmd
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/generate-parser.cmd
@@ -5,8 +5,11 @@
 
 set PATH=%PATH%;..\..\..\..\GoldBuilder
 
-GOLDBuild.exe -verbose PashGrammar.grm PashGrammar.cgt
+:: GOLDBuild.exe doesn't seem to set ERRORLEVEL, so let's check if it actually generates a file, instead.
+IF EXIST PashGrammar.cgt DEL PashGrammar.cgt
+GOLDBuild.exe PashGrammar.grm PashGrammar.cgt
 IF ERRORLEVEL 1 GOTO :EOF
+IF NOT EXIST PashGrammar.cgt goto :EOF
 
 GOLDProg.exe PashGrammar.cgt PashParserContext.pgt PashParserContext.Generated.cs
 IF ERRORLEVEL 1 GOTO :EOF
