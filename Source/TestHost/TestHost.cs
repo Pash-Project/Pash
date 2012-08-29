20e716bceb17a6a815d0b4626bf17ddfced0ac34
Jay Bazuzi <jay@bazuzi.com>
2012-08-28 20:12:59 -0700

    REFACTOR: Move class `TestHost` to its own file.

---
+++ Source/TestHost/TestHost.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation.Host;
+
+namespace TestHost
+{
+    class TestHost : PSHost
+    {
+        public override System.Globalization.CultureInfo CurrentCulture
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Globalization.CultureInfo CurrentUICulture
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Guid InstanceId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override PSHostUserInterface UI
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Version Version
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void EnterNestedPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExitNestedPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void NotifyBeginApplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void NotifyEndApplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetShouldExit(int exitCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
