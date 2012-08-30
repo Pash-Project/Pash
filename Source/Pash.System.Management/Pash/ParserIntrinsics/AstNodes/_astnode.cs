1c58da45ac25e536d201aa18e7a8f4508fedc1c0
Jay Bazuzi <jay@bazuzi.com>
2012-08-30 11:29:37 -0700

    Add `_astnode.ChildAstNodes`
    Syntactic sugar for accessing the AST nodes of children.

--- Source/Pash.System.Management/Pash/ParserIntrinsics/AstNodes/_astnode.cs
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/AstNodes/_astnode.cs
@@ -7,6 +7,7 @@ using Irony.Ast;
 using Irony.Parsing;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace Pash.ParserIntrinsics.AstNodes
 {
@@ -22,6 +23,14 @@ namespace Pash.ParserIntrinsics.AstNodes
             this.parseTreeNode = parseTreeNode;
         }
 
+        public IEnumerable<_astnode> ChildAstNodes
+        {
+            get
+            {
+                return this.parseTreeNode.ChildNodes.Select(childNode => childNode.AstNode).Cast<_astnode>();
+            }
+        }
+
         public string Text { get { return parseTreeNode.Token.Text; } }
 
         // by default, forward to the child node, if there is exactly 1. This makes sense
@@ -34,14 +43,14 @@ namespace Pash.ParserIntrinsics.AstNodes
         {
             if (this.parseTreeNode.ChildNodes.Count == 1)
             {
-                var childNode = this.parseTreeNode.ChildNodes.Single();
+                var childAstNode = this.ChildAstNodes.Single();
 
-                if (childNode.AstNode == null)
+                if (childAstNode == null)
                 {
-                    throw new NotImplementedException("AST not implemented for '{0}'. Parent node '{1}' should implement `Execute()`".FormatString(childNode, this));
+                    throw new NotImplementedException("AST not implemented for '{0}'. Parent node '{1}' should implement `Execute()`".FormatString(this.parseTreeNode.ChildNodes.Single(), this));
                 }
 
-                return ((_astnode)this.parseTreeNode.ChildNodes.Single().AstNode).Execute(context, commandRuntime);
+                return ((_astnode)childAstNode).Execute(context, commandRuntime);
             }
             else throw new NotImplementedException("AST not implemented for '{0}', because it has {1} children.. Implement `Execute()`".FormatString(this.parseTreeNode, this.parseTreeNode.ChildNodes.Count));
         }
