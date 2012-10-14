845aba3ad12b7c151ea7ed1bf5fd2b119988af04
Jay Bazuzi <JAY@BAZUZI.COM>
2012-10-13 20:08:17 -0700

    Move functions and aliases from App.config to config.ps1

--- Source/PashConsole/config.ps1
+++ Source/PashConsole/config.ps1
@@ -1,1 +1,38 @@
 function Prompt { 'PS ' + (Get-Location) + '> ' }
+function A: { Set-Location A: }
+function B: { Set-Location B: }
+function C: { Set-Location C: }
+function D: { Set-Location D: }
+function E: { Set-Location E: }
+function F: { Set-Location F: }
+function G: { Set-Location G: }
+function H: { Set-Location H: }
+function I: { Set-Location I: }
+function J: { Set-Location J: }
+function K: { Set-Location K: }
+function L: { Set-Location L: }
+function M: { Set-Location M: }
+function N: { Set-Location N: }
+function O: { Set-Location O: }
+function P: { Set-Location P: }
+function Q: { Set-Location Q: }
+function R: { Set-Location R: }
+function S: { Set-Location S: }
+function T: { Set-Location T: }
+function U: { Set-Location U: }
+function V: { Set-Location V: }
+function W: { Set-Location W: }
+function X: { Set-Location X: }
+function Y: { Set-Location Y: }
+function Z: { Set-Location Z: }
+
+Set-Alias gcm Get-Command
+Set-Alias pwd Get-Location
+Set-Alias dir Get-ChildItem
+Set-Alias ls Get-ChildItem
+Set-Alias cd Set-Location
+Set-Alias chdir Set-Location
+Set-Alias set Set-Variable
+
+Set-Alias echo Write-Output
+Set-Alias sort Sort-Object
