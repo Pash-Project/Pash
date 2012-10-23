d475f9827359324174962016176b33be2c3db9cb
Jay Bazuzi <JAY@BAZUZI.COM>
2012-10-22 22:50:39 -0700

    Implement `BinaryExpression.ToString()`

--- Source/Pash.System.Management/Pash/ParserIntrinsics/BinaryExpressionAst.cs
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/BinaryExpressionAst.cs
@@ -31,5 +31,9 @@ namespace System.Management.Automation.Language
             }
         }
 
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", this.Left, this.Operator, this.Right);
+        }
     }
 }
