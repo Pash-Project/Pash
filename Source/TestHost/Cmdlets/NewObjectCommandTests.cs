4f667e79fa6b10e7d61af79bafb64b2fe243fbc8
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-17 13:21:36 -0700

    TEST: Goal test for `New-Object`
    Currently failing.

---
+++ Source/TestHost/Cmdlets/NewObjectCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestHost.Cmdlets
+{
+    [TestFixture]
+    public class NewObjectCommandTests
+    {
+        [Test]
+        public void ATest()
+        {
+            Assert.AreEqual("xxx" + Environment.NewLine, TestHost.Execute("New-Object string 'xxx'"));
+        }
+    }
+}
