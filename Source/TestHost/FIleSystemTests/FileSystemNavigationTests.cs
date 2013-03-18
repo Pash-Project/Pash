a1e180376cbc2ad305998bf6ffb853bf0758d25d
Jason Jarrett <jason@elegantcode.com>
2013-03-17 20:21:48 -0700

    couple more test cases around path navigation

--- Source/TestHost/FIleSystemTests/FileSystemNavigationTests.cs
+++ Source/TestHost/FIleSystemTests/FileSystemNavigationTests.cs
@@ -36,18 +36,22 @@ namespace TestHost
         }
 
         [Test]
-        [TestCase("..", "/a/b/c/d/e/f", "")]
-        [TestCase("../", "/a/b/c/d/e/f", "")]
+        [TestCase(".", "/a/b/c/d/e/f/g", "should not change directories")]
+        [TestCase("./../../../../../../", "/a", "should navigate up a bunch of directories")]
+        [TestCase("./..", "/a/b/c/d/e/f", "should nav up one dir")]
+        [TestCase("./../", "/a/b/c/d/e/f", "should nav up one dir")]
+        [TestCase("..", "/a/b/c/d/e/f", "should nav up one dir")]
+        [TestCase("../", "/a/b/c/d/e/f", "should nav up one dir")]
+        [TestCase("../g/h", "/a/b/c/d/e/f/g/h", "should nav up one dir and then back down 2 dirs")]
+        [TestCase("../g/h/../../g/h", "/a/b/c/d/e/f/g/h", "should nav up one dir and then back down 2 dirs")]
         public void CDWithTwoPeriodsShouldMoveUpOneDirectory(string setLocationParam, string expectedLocation, string errorMessage)
         {
             var rootPath = base.SetupFileSystemWithStructure(new []{
                 "/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p"
             });
 
-            // set the start directory
-            TestHost.ExecuteWithZeroErrors("Set-Location " + rootPath + "/a/b/c/d/e/f/g");
-            
-            var result = TestHost.ExecuteWithZeroErrors("Set-Location " + setLocationParam,
+            var result = TestHost.ExecuteWithZeroErrors("Set-Location " + rootPath + "/a/b/c/d/e/f/g",
+                                                        "Set-Location " + setLocationParam,
                                                         "Get-Location");
             
             result.Trim().ShouldEqual(rootPath + expectedLocation);
