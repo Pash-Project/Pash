0a9cf33c719b4d41e06ff8223356a0a32eb4de89
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-20 19:31:04 -0700

    Add copyright/license messages to all C# sources.

--- Source/Pash.System.Management/Pash/Implementation/ExecutionVisitor.cs
+++ Source/Pash.System.Management/Pash/Implementation/ExecutionVisitor.cs
@@ -1,4 +1,51 @@
-﻿using System;
+// Copyright (C) Pash Contributors. All Rights Reserved. See https://github.com/Pash-Project/Pash/
+
+#region BSD License
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and documentation are
+// those of the authors and should not be interpreted as representing official
+// policies, (either expressed or implied, of the FreeBSD Project.
+#endregion
+
+#region GPL License
+// This file is part of Pash.
+//
+// Pash is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Pash is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Pash.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -122,7 +169,7 @@ namespace System.Management.Pash.Implementation
             ////
             //// A range-expression creates an unconstrained 1-dimensional array whose elements are the values of 
             //// the int sequence specified by the range bounds. The values designated by the operands are converted 
-            //// to int, if necessary (§6.4). 
+            //// to int, if necessary (?6.4). 
 
             //// The operand designating the lower value after conversion is the lower 
             //// bound, while the operand designating the higher value after conversion is the upper bound. 
@@ -144,9 +191,9 @@ namespace System.Management.Pash.Implementation
             //// [Note: Conceptually, this operator is a shortcut for the corresponding binary comma operator 
             //// sequence. For example, the range 5..8 can also be generated using 5,6,7,8. However, if an ascending 
             //// or descending sequence is needed without having an array, an implementation may avoid generating an 
-            //// actual array. For example, in foreach ($i in 1..5) { … }, no array need be created. end note]
+            //// actual array. For example, in foreach ($i in 1..5) { ? }, no array need be created. end note]
             ////
-            //// A range-expression can be used to specify an array slice (§9.9).
+            //// A range-expression can be used to specify an array slice (?9.9).
 
             throw new Exception("unreachable");
         }
@@ -218,7 +265,7 @@ namespace System.Management.Pash.Implementation
             ////  7.7.1 Addition
             ////      Description:
             ////      
-            ////          The result of the addition operator + is the sum of the values designated by the two operands after the usual arithmetic conversions (§6.15) have been applied.
+            ////          The result of the addition operator + is the sum of the values designated by the two operands after the usual arithmetic conversions (?6.15) have been applied.
             ////      
             ////          This operator is left associative.
             ////      
