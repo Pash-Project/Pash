e01102543fb7278468f6a65ffcc6026449f3b712
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-22 05:33:01 -0700

    TEST: Use `TestCase` attribute in parser tests.

--- Source/ParserTests/ParserTests.cs
+++ Source/ParserTests/ParserTests.cs
@@ -11,54 +11,13 @@ namespace ParserTests
     public class ParserTests
     {
         [Test]
-        public void IfTest()
+        [TestCase(@"if ($true) {} else {}")]
+        [TestCase(@"if ($true) {} elseif ($true) {} ")]
+        [TestCase(@"if ($true) {} elseif {$true) {} else {}", Explicit = true)]
+        [TestCase(@"if ($true) {} elseif ($true) {} elseif ($true) else {}", Explicit = true)]
+        public void IfElseSyntax(string input)
         {
-            var parseTree = PowerShellGrammar.Parser.Parse(@"if ($true) {}");
-
-            if (parseTree.HasErrors())
-            {
-                Assert.Fail(parseTree.ParserMessages[0].ToString());
-            }
-        }
-
-        [Test]
-        public void IfElseTest()
-        {
-            var parseTree = PowerShellGrammar.Parser.Parse(@"if ($true) {} else {}");
-
-            if (parseTree.HasErrors())
-            {
-                Assert.Fail(parseTree.ParserMessages[0].ToString());
-            }
-        }
-
-        [Test]
-        public void IfElseIfTest()
-        {
-            var parseTree = PowerShellGrammar.Parser.Parse(@"if ($true) {} elseif ($true) {} ");
-
-            if (parseTree.HasErrors())
-            {
-                Assert.Fail(parseTree.ParserMessages[0].ToString());
-            }
-        }
-
-        [Test, Explicit("bug")]
-        public void IfElseIfElseTest()
-        {
-            var parseTree = PowerShellGrammar.Parser.Parse(@"if ($true) {} elseif {$true) {} else {}");
-
-            if (parseTree.HasErrors())
-            {
-                Assert.Fail(parseTree.ParserMessages[0].ToString());
-            }
-        }
-
-
-        [Test, Explicit("bug")]
-        public void IfElseifElseTest()
-        {
-            var parseTree = PowerShellGrammar.Parser.Parse(@"if ($true) {} elseif ($true) {} elseif ($true) else {}");
+            var parseTree = PowerShellGrammar.Parser.Parse(input);
 
             if (parseTree.HasErrors())
             {
