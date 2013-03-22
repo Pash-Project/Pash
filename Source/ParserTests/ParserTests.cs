c8f60a31e6b31efdc511a4948e58a305480517e0
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-22 05:34:51 -0700

    Parsing test cases for type literals and member access.

--- Source/ParserTests/ParserTests.cs
+++ Source/ParserTests/ParserTests.cs
@@ -24,5 +24,25 @@ namespace ParserTests
                 Assert.Fail(parseTree.ParserMessages[0].ToString());
             }
         }
+
+        [Test]
+        [TestCase(@"[int]")]
+        [TestCase(@"[int]-3"), Description("Cast, not subtraction")]
+        [TestCase(@"[int],[string]")]
+        [TestCase(@"$x = [int]")]
+        [TestCase(@"$x::MaxValue")]
+        [TestCase(@"[int]::Parse()", Explicit = true)]
+        [TestCase(@"$x::Parse()", Explicit = true)]
+        [TestCase(@"$x.Assembly")]
+        [TestCase(@"$x.AsType()", Explicit = true)]
+        public void TypesAndMembers(string input)
+        {
+            var parseTree = PowerShellGrammar.Parser.Parse(input);
+
+            if (parseTree.HasErrors())
+            {
+                Assert.Fail(parseTree.ParserMessages[0].ToString());
+            }
+        }
     }
 }
