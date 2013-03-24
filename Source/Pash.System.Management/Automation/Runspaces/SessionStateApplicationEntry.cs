d633280a5dd236c68feb7ca4fb0490a80237e73e
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-24 09:56:50 -0700

    Add missing classes SessionStateApplicationEntry, SessionStateScriptEntry

---
+++ Source/Pash.System.Management/Automation/Runspaces/SessionStateApplicationEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Management.Automation.Runspaces
+{
+    public sealed class SessionStateApplicationEntry : SessionStateCommandEntry
+    {
+        private string path;
+
+        public string Path
+        {
+            get
+            {
+                return this.path;
+            }
+        }
+
+        public SessionStateApplicationEntry(string path)
+            : base(path, SessionStateEntryVisibility.Public)
+        {
+            this.path = path;
+            this.commandType = CommandTypes.Application;
+        }
+
+        public override InitialSessionStateEntry Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
