f1e2b2514cc74457c1b506628c9328303f70ad2e
Jay Bazuzi <JAY@BAZUZI.COM>
2013-04-14 22:35:25 -0700

    Fix tokenizing of binary operators (e.g. `-or`)
    We need to mark binary operators as high priority, so they get recogized as operators.
    
    See commit 522b1b2adf09dcc46ce03bd7a8314d301164c69b to understand what is going on.

--- Source/System.Management/Pash/ParserIntrinsics/PowerShellGrammar.BinaryOperatorTerminal.cs
+++ Source/System.Management/Pash/ParserIntrinsics/PowerShellGrammar.BinaryOperatorTerminal.cs
@@ -22,5 +22,17 @@ namespace Pash.ParserIntrinsics
                 Priority = TerminalPriority.ReservedWords;
             }
         }
+
+        ////            dash   and				dash   band				dash   bnot
+        ////            dash   bor				dash   bxor				dash   not
+        ////            dash   or				dash   xor
+        public readonly BinaryOperatorTerminal _operator_and = new BinaryOperatorTerminal("and", TokenKind.And);
+        public readonly BinaryOperatorTerminal _operator_band = new BinaryOperatorTerminal("band", TokenKind.Band);
+        public readonly BinaryOperatorTerminal _operator_bnot = new BinaryOperatorTerminal("bnot", TokenKind.Bnot);
+        public readonly BinaryOperatorTerminal _operator_bor = new BinaryOperatorTerminal("bor", TokenKind.Bor);
+        public readonly BinaryOperatorTerminal _operator_bxor = new BinaryOperatorTerminal("bxor", TokenKind.Bxor);
+        public readonly BinaryOperatorTerminal _operator_not = new BinaryOperatorTerminal("not", TokenKind.Not);
+        public readonly BinaryOperatorTerminal _operator_or = new BinaryOperatorTerminal("or", TokenKind.Or);
+        public readonly BinaryOperatorTerminal _operator_xor = new BinaryOperatorTerminal("xor", TokenKind.Xor);
     }
 }
