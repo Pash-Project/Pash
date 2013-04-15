6cfde07c037a9115099d12413692213af593c861
Jay Bazuzi <JAY@BAZUZI.COM>
2013-04-14 22:16:43 -0700

    REFACTOR: Extract base class `BinaryOperatorTerminal` from `ComparisonOperatorTerminal`

---
+++ Source/System.Management/Pash/ParserIntrinsics/PowerShellGrammar.BinaryOperatorTerminal.cs
@@ -0,0 +1,26 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation.Language;
+using System.Text;
+
+namespace Pash.ParserIntrinsics
+{
+    partial class PowerShellGrammar
+    {
+        public class BinaryOperatorTerminal : RegexBasedTerminal
+        {
+            public readonly string Operator;
+            public readonly TokenKind TokenKind;
+
+            public BinaryOperatorTerminal(string @operator, TokenKind tokenKind)
+                : base("-" + @operator, "(?<_operator_" + @operator + ">" + dash_pattern + @operator + ")")
+            {
+                this.Operator = @operator;
+                this.TokenKind = tokenKind;
+                Priority = TerminalPriority.ReservedWords;
+            }
+        }
+    }
+}
