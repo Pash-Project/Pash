60826d449488fbe19c81c47bd08f93386392b958
Jay Bazuzi <JAY@BAZUZI.COM>
2013-04-28 14:46:36 -0700

    Minor: use `Single()` instead of `[0]`

--- Source/System.Management/Automation/Language/Ast.cs
+++ Source/System.Management/Automation/Language/Ast.cs
@@ -23,7 +23,7 @@ namespace System.Management.Automation.Language
         {
             var dispatchMethodInfos = from dmi in astVisitor.GetType().GetMethods()
                                       where dmi.Name.StartsWith("Visit")
-                                      where dmi.GetParameters()[0].ParameterType.IsAssignableFrom(ast)
+                                      where dmi.GetParameters().Single().ParameterType.IsAssignableFrom(ast)
                                       select dmi;
 
             // TODO: Find out what PowerShell does when there's more than one match.
