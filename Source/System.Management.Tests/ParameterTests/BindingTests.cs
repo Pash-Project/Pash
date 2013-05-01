193e08467e218a4fae24c4aedc31d977452b5c5b
Andrew Barnes <barnes333@gmail.com>
2013-05-02 08:35:58 +1000

    Remove spurious comments

--- Source/System.Management.Tests/ParameterTests/BindingTests.cs
+++ Source/System.Management.Tests/ParameterTests/BindingTests.cs
@@ -1,6 +1,4 @@
 // Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
-
-//classtodo: Implement, may require runtime improvements.
 using System;
 using System.Collections.Generic;
 using System.Linq;
