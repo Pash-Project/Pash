ba0b5853be77425dec4825aab6fecf2b4a152ba2
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2013-07-03 10:49:10 +1000

    Implement RunspaceConfiguration.Providers
    Corrects spaces -> TABS

--- Source/System.Management/Automation/Runspaces/RunspaceConfiguration.cs
+++ Source/System.Management/Automation/Runspaces/RunspaceConfiguration.cs
@@ -4,121 +4,106 @@ using System.Management.Automation;
 
 namespace System.Management.Automation.Runspaces
 {
-    public abstract class RunspaceConfiguration
-    {
-        private RunspaceConfigurationEntryCollection<CmdletConfigurationEntry> cmdlets;
+	public abstract class RunspaceConfiguration
+	{
+		private RunspaceConfigurationEntryCollection<CmdletConfigurationEntry> cmdlets;
+		private RunspaceConfigurationEntryCollection<ProviderConfigurationEntry> providers;
 
-        public abstract string ShellId
-        {
-            get;
-        }
+		public abstract string ShellId {
+			get;
+		}
 
-        protected RunspaceConfiguration()
-        {
-        }
+		protected RunspaceConfiguration ()
+		{
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<CmdletConfigurationEntry> Cmdlets
-        {
-            get
-            {
-                if (this.cmdlets == null)
-                {
-                    this.cmdlets = new RunspaceConfigurationEntryCollection<CmdletConfigurationEntry>();
-                }
+		public virtual RunspaceConfigurationEntryCollection<CmdletConfigurationEntry> Cmdlets {
+			get {
+				if (this.cmdlets == null) {
+					this.cmdlets = new RunspaceConfigurationEntryCollection<CmdletConfigurationEntry> ();
+				}
 
-                return this.cmdlets;
-            }
-        }
+				return this.cmdlets;
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<ProviderConfigurationEntry> Providers
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<ProviderConfigurationEntry> Providers {
+			get {
+				if (this.providers == null) {
+					this.providers = new RunspaceConfigurationEntryCollection<ProviderConfigurationEntry> ();
+				}
 
-        internal TypeTable TypeTable
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+				return this.providers;
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<TypeConfigurationEntry> Types
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		internal TypeTable TypeTable {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<FormatConfigurationEntry> Formats
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<TypeConfigurationEntry> Types {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<ScriptConfigurationEntry> Scripts
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<FormatConfigurationEntry> Formats {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<ScriptConfigurationEntry> InitializationScripts
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<ScriptConfigurationEntry> Scripts {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public virtual RunspaceConfigurationEntryCollection<AssemblyConfigurationEntry> Assemblies
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<ScriptConfigurationEntry> InitializationScripts {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public virtual AuthorizationManager AuthorizationManager
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+		public virtual RunspaceConfigurationEntryCollection<AssemblyConfigurationEntry> Assemblies {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public static RunspaceConfiguration Create(string assemblyName)
-        {
-            return null;
-        }
+		public virtual AuthorizationManager AuthorizationManager {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
 
-        public static RunspaceConfiguration Create(string consoleFilePath, out PSConsoleLoadException warnings)
-        {
-            warnings = null;
-            return null;
-        }
+		public static RunspaceConfiguration Create (string assemblyName)
+		{
+			return null;
+		}
 
-        public static RunspaceConfiguration Create()
-        {
-            return RunspaceFactory.DefaultRunspaceConfiguration;
-        }
+		public static RunspaceConfiguration Create (string consoleFilePath, out PSConsoleLoadException warnings)
+		{
+			warnings = null;
+			return null;
+		}
 
-        public PSSnapInInfo AddPSSnapIn(string name, out PSSnapInException warning)
-        {
-            throw new NotImplementedException();
-        }
+		public static RunspaceConfiguration Create ()
+		{
+			return RunspaceFactory.DefaultRunspaceConfiguration;
+		}
 
-        public PSSnapInInfo RemovePSSnapIn(string name, out PSSnapInException warning)
-        {
-            throw new NotImplementedException();
-        }
+		public PSSnapInInfo AddPSSnapIn (string name, out PSSnapInException warning)
+		{
+			throw new NotImplementedException ();
+		}
 
-    }
+		public PSSnapInInfo RemovePSSnapIn (string name, out PSSnapInException warning)
+		{
+			throw new NotImplementedException ();
+		}
+
+	}
 }
