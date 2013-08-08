0ca5f6879139f2bc46bb0d1b872403adf22db72e
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2013-08-08 13:49:07 +1000

    Add class ProgressRecord stubs

--- Source/System.Management/Automation/ProgressRecord.cs
+++ Source/System.Management/Automation/ProgressRecord.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,17 +7,106 @@ namespace System.Management.Automation
 {
     public class ProgressRecord
     {
-        //public ProgressRecord(int activityId, string activity, string statusDescription);
+        public int ActivityId
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+		}
 
-        //public string Activity { get; set; }
-        //public int ActivityId { get; }
-        //public string CurrentOperation { get; set; }
-        //public int ParentActivityId { get; set; }
-        //public int PercentComplete { get; set; }
-        //public ProgressRecordType RecordType { get; set; }
-        //public int SecondsRemaining { get; set; }
-        //public string StatusDescription { get; set; }
+		public int ParentActivityId
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
 
-        //public override string ToString();
+		public string Activity
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public string StatusDescription
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public string CurrentOperation
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int PercentComplete
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public int SecondsRemaining
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public ProgressRecordType RecordType
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public ProgressRecord(int activityId, string activity, string statusDescription)
+		{
+
+		}
+
+		public override string ToString()
+		{
+			throw new NotImplementedException ();
+		}
     }
 }
