264b4a02f04d5ac3eb57077135d51fc064169f2a
Matt Ward <ward.matt@gmail.com>
2013-09-23 15:28:49 +0100

    Implement combining paths for join-path.
    Simple implementation to support directory and file paths. Join-Path should be calling
    NavigationCmdletProvider.MakePath(string parent, string child) but it is not
    doing this currently:
    
    http://msdn.microsoft.com/en-us/library/windows/desktop/ee126197.aspx

--- Source/Microsoft.PowerShell.Commands.Management/JoinPathCommand.cs
+++ Source/Microsoft.PowerShell.Commands.Management/JoinPathCommand.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.ObjectModel;
+using System.Management;
 using System.Management.Automation;
 
 namespace Microsoft.PowerShell.Commands
@@ -13,6 +14,10 @@ namespace Microsoft.PowerShell.Commands
     {
         protected override void ProcessRecord()
         {
+            foreach (Path parentPath in Path)
+            {
+                WriteObject(parentPath.Combine(ChildPath));
+            }
         }
 
         [Parameter(
---
+++ Source/TestHost/Cmdlets/JoinPathTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+using NUnit.Framework;
+using System;
+
+namespace TestHost.Cmdlets
+{
+    [TestFixture]
+    public class JoinPathTests
+    {
+        [Test]
+        public void OneParentFolderAndChildFolder()
+        {
+            string result = TestHost.Execute(@"Join-Path 'parent' 'child'");
+
+            Assert.AreEqual(@"parent\child" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void TwoParentFoldersAndOneChildFolder()
+        {
+            string result = TestHost.Execute(@"Join-Path parent1,parent2 child");
+
+            Assert.AreEqual(string.Format(@"parent1\child{0}parent2\child{0}", Environment.NewLine), result);
+        }
+    }
+}
