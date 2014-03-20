53ba61253ce5130a5698495fa546c3cda76295c7
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-03-20 11:06:37 +0100

    Small Fixes

--- Source/System.Management/Automation/Runspaces/CommandParameter.cs
+++ Source/System.Management/Automation/Runspaces/CommandParameter.cs
@@ -18,7 +18,7 @@ namespace System.Management.Automation.Runspaces
         public string Name { get; private set; }
         public object Value { get; private set; }
 
-        public string ToString()
+        public override string ToString()
         {
             return Name + "=" + Value;
         }
