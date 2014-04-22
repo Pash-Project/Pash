11cd93181cd22580fd2363e3d357c81872b58222
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-04-22 23:39:34 +0200

    Made test explicit again which didn't work correct.

--- Source/TestHost/Tests.cs
+++ Source/TestHost/Tests.cs
@@ -456,12 +456,12 @@ namespace TestHost
             Assert.AreEqual("11" + Environment.NewLine, result);
         }
 
-        [Test]
+        [Test, Explicit("Does currently not work")]
         public void ForEachCharacterInString()
         {
             string result = TestHost.Execute("foreach ($char in 'abc') { $char }");
 
-            Assert.AreEqual(string.Format("a{0}b{0}c{0}", Environment.NewLine), result);
+            Assert.AreEqual("abc" + Environment.NewLine, result);
         }
 
         [Test]
