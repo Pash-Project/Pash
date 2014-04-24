944c2f14549ce6cbf849590df4df81c7d37bc9bf
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-04-24 10:17:02 +0200

    Fix: Invocation of methods in old mono versions
    Old mono versions didn't support optional "params" parameters
    when invoking methods using reflection.

--- Source/System.Management/Automation/PSMethod.cs
+++ Source/System.Management/Automation/PSMethod.cs
@@ -133,8 +133,17 @@ namespace System.Management.Automation
             var methodInfo = _classType.GetMethod(Name, argTypes);
             if (methodInfo != null && methodInfo.IsPublic && methodInfo.IsStatic != IsInstance)
             {
-                newArguments = arguments;
-                return methodInfo;
+                // this doesn't mean that we got all arguments for methodInfo, e.g. we'd have a
+                // methodInfo for a method that takes an optional argument although argTypes doesn't
+                // include it. As old mono versions don't support invoking with optional arguments
+                // we need to take care of it
+                object[] fittingArgs;
+                if (MethodFitsArgs(methodInfo, arguments, out fittingArgs))
+                {
+                    newArguments = fittingArgs;
+                    return methodInfo;
+                }
+                // if we weren't successful (shouldn't be the case, but who knows), we try all overloads
             }
 
             // then check methods with conersion, optionall parameters or "params" parameter
