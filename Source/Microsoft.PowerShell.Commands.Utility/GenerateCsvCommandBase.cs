0da1ef0c6503aff7cf8420f4d38378945d97decf
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-05-22 16:17:50 +0200

    Fixed and implemented CSV cmdlets

---
+++ Source/Microsoft.PowerShell.Commands.Utility/GenerateCsvCommandBase.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Microsoft.PowerShell.Commands
+{
+    public class GenerateCsvCommandBase : PSCmdlet
+    {
+        private List<string> _header;
+
+        /// <summary>
+        /// Specifies a delimiter to separate the property values. The default is a comma (,).
+        /// </summary>
+        [Parameter(Position = 2)]
+        public char Delimiter { get; set; }
+
+        /// <summary>
+        /// The object to be exported as a CSV file.
+        /// </summary>
+        [Parameter(ValueFromPipeline = true, Mandatory = true, Position = 1)]
+        public PSObject InputObject { get; set; }
+
+        /// <summary>
+        /// Omits type information from the generated CSV file. By the default the first line of the CSV file contains type information.
+        /// </summary>
+        [Parameter]
+        public SwitchParameter NoTypeInformation { get; set; }
+
+        protected List<string> ProcessObject(PSObject obj)
+        {
+            var lines = new List<string>();
+            char delim = Delimiter == 0 ? ',' : Delimiter;
+
+            if (_header == null)
+            {
+                _header = new List<string>();
+                if (!NoTypeInformation.IsPresent)
+                {
+                    lines.Add("#TYPE " + InputObject.GetType().FullName);
+                }
+                foreach (PSPropertyInfo prop in InputObject.Properties)
+                {
+                    _header.Add(prop.Name);
+                }
+                var escaped = (from h in _header select String.Format("\"{0}\"", h)).ToArray();
+                lines.Add(String.Join(delim.ToString(), escaped));
+            }
+
+            StringBuilder line = new StringBuilder();
+            foreach (var propName in _header)
+            {
+                var prop = InputObject.Properties[propName];
+                if (prop != null)
+                {
+                    line.AppendFormat("\"{0}\"", prop.Value);
+                }
+                line.Append(delim);
+            }
+            // Remove the trailing comma
+            line.Remove((line.Length - 1), 1);
+            lines.Add(line.ToString());
+
+            return lines;
+        }
+    }
+}
+
--- Source/Microsoft.PowerShell.Commands.Utility/ImportCsvCommand.cs
+++ Source/Microsoft.PowerShell.Commands.Utility/ImportCsvCommand.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.IO;
 using System.Collections.Generic;
 using System.Collections;
+using System.Net.Configuration;
 
 namespace Microsoft.PowerShell.Commands
 {
@@ -24,53 +25,148 @@ namespace Microsoft.PowerShell.Commands
     /// RELATED POSIX COMMANDS
     ///   n/a 
     /// </summary>
-    [Cmdlet("Import", "Csv")]
+    [Cmdlet(VerbsData.Import, "Csv")]
     public sealed class ImportCsvCommand : PSCmdlet
     {
-        //todo
-        protected override void ProcessRecord()
-        {
-            foreach (String _path in Path)
-            {
-                StreamReader _file = new StreamReader(_path);
-                PSObject _obj = new PSObject();
-
-                if (_file.Peek() == '#')
-                {
-                    String _line = _file.ReadLine();
-                    if (_line.StartsWith("#TYPE"))
-                    {
-                        _obj.TypeNames.Add(_line.Substring(5).Trim());
-                    }
-                }
-
-
-                // Hold the names of the properties, the capacity is pretty arbitrary
-                //List<String> _names = new List<String>(16);
-
-                //bool inValues = false;
-
-                while (!_file.EndOfStream)
-                {
-                    char _ch = (char)_file.Read();
-                }
-                /* if ((_ch == ',')
-                 {
-                     _obj.Properties.Add(new PSNoteProperty());
-                 } */
-            }
-        }
-
+        private System.Text.Encoding _useEncoding;
+        
         /// <summary>
         /// The path of the CSV file to import.
         /// </summary>
-        [Parameter(Position = 0,
-            Mandatory = true,
-            ValueFromPipeline = true,
-            ValueFromPipelineByPropertyName = true),
-        Alias(new string[] { "PSPath" })]
+        [Parameter(Position = 1,
+                   Mandatory = true,
+                   ValueFromPipeline = true),
+         Alias(new string[] { "PSPath" })]
         public String[] Path { get; set; }
 
+        /// <summary>
+        /// Specifies an alternate column header row for the imported file.
+        /// </summary>
+        [Parameter]
+        public String[] Header { get; set; }
+
+        /// <summary>
+        /// Specifies a delimiter to separate the property values. The default is a comma (,).
+        /// </summary>
+        [Parameter(Position = 2)]
+        public char Delimiter { get; set; }
+
+        /// <summary>
+        /// What encoding you want the input file is in.
+        /// </summary>
+        [Parameter,
+         ValidateSet(new string[] { "Unicode", "UTF7", "UTF8", "ASCII", "UTF32", "BigEndianUnicode", "Default", "OEM" })]
+        public string Encoding { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            foreach (String curPath in Path)
+            {
+                var file = OpenFile(curPath);
+                string typename = "";
+                if (file.Peek() == '#')
+                {
+                    var typeline = file.ReadLine();
+                    if (typeline.StartsWith("#TYPE"))
+                    {
+                        typename = typeline.Substring(5).Trim();
+                    }
+                }
+
+                string[] useHeader;
+                if (Header != null && Header.Length > 0)
+                {
+                    useHeader = Header;
+                }
+                else
+                {
+                    var headerLine = file.ReadLine();
+                    if (headerLine == null)
+                    {
+                        return;
+                    }
+                    // first line is property names
+                    useHeader = ParseValuesFromLine(headerLine).ToArray();
+                }
+
+                string line;
+                for (line = file.ReadLine(); line != null; line = file.ReadLine())
+                {
+                    PSObject obj = new PSObject();
+                    if (!String.IsNullOrEmpty(typename))
+                    {
+                        obj.TypeNames.Add(typename);
+                    }
+                    var values = ParseValuesFromLine(line);
+                    for (int i = 0; i < useHeader.Length; i++)
+                    {
+                        string value = (i < values.Count) ? values[i] : "";
+                        var psprop = new PSNoteProperty(useHeader[i], value);
+                        obj.Properties.Add(psprop);
+                        obj.Members.Add(psprop);
+                    }
+                    WriteObject(obj);
+                }
+                file.Close();
+            }
+        }
+
+        private StreamReader OpenFile(string path)
+        {
+            if (_useEncoding == null)
+            {
+                _useEncoding = System.Text.Encoding.ASCII;
+                if (!String.IsNullOrEmpty(Encoding))
+                {
+                    try
+                    {
+                        _useEncoding = System.Text.Encoding.GetEncoding(Encoding);
+                    }
+                    catch (ArgumentException)
+                    {
+                        // shouldn't happen as Encoding gets validated
+                        var msg = String.Format("Invalid encoding '{0}'", Encoding);
+                        ThrowTerminatingError(new PSArgumentException(msg).ErrorRecord);
+                    }
+                } 
+            }
+            return new StreamReader(path, _useEncoding);
+        }
+
+        private List<string> ParseValuesFromLine(string line)
+        {
+            var values = new List<string>();
+            char delim = Delimiter == 0 ? ',' : Delimiter;
+            var curValue = new StringBuilder();
+            bool waitForQuote = false;
+            bool inValue = false;
+            // Powershell behavior: skip only double quotes at the beginning of a value and ignore delimiters
+            // until the next double quotes
+            foreach (char c in line)
+            {
+                if (!inValue && c == '"')
+                {
+                    waitForQuote = true;
+                    continue;
+                }
+                inValue = true;
+                if (!waitForQuote && c == delim)
+                {
+                    values.Add(curValue.ToString());
+                    curValue.Clear();
+                    inValue = false;
+                    continue;
+                }
+                if (c == '"')
+                {
+                    waitForQuote = false;
+                    continue;
+                }
+                curValue.Append(c);
+            }
+            values.Add(curValue.ToString()); // last value still in builder (no trailing delimiter)
+            return values;
+        }
 
     }
 }
