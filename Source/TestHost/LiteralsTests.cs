8778eb3a4de5d715de7967ffbd3489c25a288a78
Matt Ward <ward.matt@gmail.com>
2014-05-24 12:48:20 +0100

    Fix incorrect conversion of double in string on non-US locale.
    Add tests for real literal output on non-US locales.

--- Source/TestHost/LiteralsTests.cs
+++ Source/TestHost/LiteralsTests.cs
@@ -8,7 +8,6 @@ using NUnit.Framework;
 namespace TestHost
 {
     [TestFixture]
-    [SetCulture("en-US")]
     public class LiteralsTests
     {
         ////  7.7.1 Addition
@@ -19,6 +18,7 @@ namespace TestHost
         ////          This operator is left associative.
         ////      
         [Test(Description = "from the spec")]
+        [SetCulture("en-US")]
         public void AdditionExamples()
         {
             ////          12 + -10L               # long result 2
@@ -29,6 +29,23 @@ namespace TestHost
 
             ////          10.6 + 12               # double result 22.6
             Assert.AreEqual("22.6" + Environment.NewLine, TestHost.Execute("10.6 + 12"));
+
+            ////          12 + "0xabc"            # int result 2760
+            Assert.AreEqual("2760" + Environment.NewLine, TestHost.Execute("12 + \"0xabc\""));
+        }
+
+        [Test(Description = "from the spec")]
+        [SetCulture("de-DE")]
+        public void GermanLocaleAdditionExamples()
+        {
+            ////          12 + -10L               # long result 2
+            Assert.AreEqual("2" + Environment.NewLine, TestHost.Execute("12 + -10L"));
+
+            ////          -10.300D + 12           # decimal result 1.700
+            Assert.AreEqual("1,700" + Environment.NewLine, TestHost.Execute("-10.300D + 12"));
+
+            ////          10.6 + 12               # double result 22.6
+            Assert.AreEqual("22,6" + Environment.NewLine, TestHost.Execute("10.6 + 12"));
 
             ////          12 + "0xabc"            # int result 2760
             Assert.AreEqual("2760" + Environment.NewLine, TestHost.Execute("12 + \"0xabc\""));
@@ -53,6 +70,7 @@ namespace TestHost
         }
 
         [Test]
+        [SetCulture("en-US")]
         [TestCase("1.1 + 2.3", "3.4")]
         [TestCase("1 + 0.4", "1.4")]
         [TestCase("0.4 + 1", "1.4")]
@@ -68,6 +86,27 @@ namespace TestHost
         [TestCase("12 + 10.300D", "22.300")]
         [TestCase("12.1 + 10.300D", "22.400")]
         public void AddReals(string input, string result)
+        {
+            Assert.AreEqual(result + Environment.NewLine, TestHost.Execute(input));
+        }
+
+        [Test]
+        [SetCulture("de-DE")]
+        [TestCase("1.1 + 2.3", "3,4")]
+        [TestCase("1 + 0.4", "1,4")]
+        [TestCase("0.4 + 1", "1,4")]
+        [TestCase("1.1 + \"2.3\"", "3,4")]
+        [TestCase("1.1 + 0xab", "172,1")]
+        [TestCase("0xab + 1.1", "172,1")]
+        [TestCase("-1.1 + 2.3", "1,2")]
+        [TestCase("1KB + 2.2", "1026,2")]
+        [TestCase("2.2 + 1KB", "1026,2")]
+        [TestCase("1.1kb + 1.2kb", "2355,2")]
+        [TestCase("-10.300D + 12", "1,700")]
+        [TestCase("-10.300D + 12.1", "1,800")]
+        [TestCase("12 + 10.300D", "22,300")]
+        [TestCase("12.1 + 10.300D", "22,400")]
+        public void GermanLocaleAddReals(string input, string result)
         {
             Assert.AreEqual(result + Environment.NewLine, TestHost.Execute(input));
         }
