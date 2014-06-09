31c5baf671fc341a22654a49779e16a0a7b9766a
Carl Walsh <darthwalsh@gmail.com>
2014-06-08 17:47:25 -0700

    Add cmdlet Split-Path and tests

---
+++ Source/Microsoft.PowerShell.Commands.Management/SplitPathCommand.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+
+//classtodo: Currently borked, needs Pash.Engine implementation
+
+using System;
+using System.Collections.ObjectModel;
+using System.Management;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.Commands
+{
+    [Cmdlet("Split", "Path")]
+    public class SplitPathCommand : ProviderCommandBase
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (Path path in Path)
+            {
+                WriteObject(path.GetParentPath(string.Empty).ToString());
+            }
+        }
+
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true),
+        Alias(new string[] { "PSPath" })]
+        public string[] Path { get; set; }
+
+        [Parameter]
+        public SwitchParameter Resolve { get; set; }
+    }
+}
---
+++ Source/TestHost/Cmdlets/SplitPathTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+using NUnit.Framework;
+using System;
+
+namespace TestHost.Cmdlets
+{
+    [TestFixture]
+    public class SplitPathTests
+    {
+        [Test]
+        [Platform("Win")]
+        public void OneParentFolderUnderWindows()
+        {
+            string result = TestHost.Execute(@"Split-Path 'parent\child'");
+
+            Assert.AreEqual(@"parent" + Environment.NewLine, result);
+        }
+
+        [Test]
+        [Platform("Unix")]
+        public void OneParentFolderUnderUnix()
+        {
+            string result = TestHost.Execute(@"Split-Path 'parent/child'");
+
+            Assert.AreEqual(@"parent" + Environment.NewLine, result);
+        }
+
+        [Test]
+        [Platform("Win")]
+        public void TwoParentFoldersUnderWindows()
+        {
+            string result = TestHost.Execute(@"Split-Path parent1\child,parent2\child");
+
+            Assert.AreEqual(string.Format(@"parent1{0}parent2{0}", Environment.NewLine), result);
+        }
+
+        [Test]
+        [Platform("Unix")]
+        public void TwoParentFoldersUnderUnix()
+        {
+            string result = TestHost.Execute(@"Split-Path parent1/child,parent2/child");
+
+            Assert.AreEqual(string.Format(@"parent1{0}parent2{0}", Environment.NewLine), result);
+        }
+    }
+}
