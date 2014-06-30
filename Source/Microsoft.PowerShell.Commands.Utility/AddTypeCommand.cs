9c74c44c131a9f0099a76105bd0b88c92bf69e41
Matt Ward <ward.matt@gmail.com>
2014-06-30 22:45:07 +0100

    Implement Add-Type -AssemblyName
    Allows the use of types from assemblies that are not available by
    default.
    
      Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
      [Microsoft.Build.Evaluation.ProjectCollection]

---
+++ Source/Microsoft.PowerShell.Commands.Utility/AddTypeCommand.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Reflection;
+using Microsoft.PowerShell.Commands.Utility;
+
+namespace Microsoft.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Add, "Type", DefaultParameterSetName = "FromSource"
+        /*HelpUri="http://technet.microsoft.com/en-us/library/hh849914.aspx",
+                 RemotingCapability=RemotingCapability.None*/)] 
+    public class AddTypeCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            ParameterSetName = "FromAssemblyName")] 
+        public string[] AssemblyName { get; set; }
+
+        [Parameter(ParameterSetName = "FromSource")] 
+        [Parameter(ParameterSetName = "FromMember")] 
+        public CodeDomProvider CodeDomProvider { get; set; }
+
+        [Parameter(ParameterSetName = "FromSource")]
+        [Parameter(ParameterSetName = "FromMember")]
+        [Parameter(ParameterSetName = "FromPath")]
+        [Parameter(ParameterSetName = "FromLiteralPath")] 
+        public CompilerParameters CompilerParameters { get; set; }
+
+        [Parameter]
+        public SwitchParameter IgnoreWarnings { get; set; }
+
+        [Parameter(ParameterSetName = "FromMember")]
+        [Parameter(ParameterSetName = "FromSource")] 
+        public Language Language { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ParameterSetName = "FromLiteralPath")] 
+        public string[] LiteralPath { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ParameterSetName = "FromMember")]
+        public string MemberDefinition { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ParameterSetName = "FromMember")] 
+        public string Name { get; set; }
+
+        [Parameter(ParameterSetName = "FromMember")] 
+        [AllowNull]
+        public string Namespace { get; set; }
+
+        [Parameter(ParameterSetName = "FromMember")]
+        [Parameter(ParameterSetName = "FromPath")]
+        [Parameter(ParameterSetName = "FromLiteralPath")]
+        [Parameter(ParameterSetName = "FromSource")]
+        public string OutputAssembly { get; set; }
+
+        [Parameter(ParameterSetName = "FromLiteralPath")] 
+        [Parameter(ParameterSetName = "FromMember")] 
+        [Parameter(ParameterSetName = "FromPath")] 
+        [Parameter(ParameterSetName = "FromSource")] 
+        public OutputAssemblyType OutpuType { get; set; }
+
+        [Parameter]
+        public SwitchParameter PassThru { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ParameterSetName = "FromPath")]
+        public string[] Path { get; set; }
+
+        [Parameter(ParameterSetName = "FromLiteralPath")]
+        [Parameter(ParameterSetName = "FromPath")]
+        [Parameter(ParameterSetName = "FromMember")]
+        [Parameter(ParameterSetName = "FromSource")]
+        public string[] ReferencedAssemblies { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ParameterSetName = "FromSource")]
+        public string TypeDefinition { get; set; }
+
+        [ParameterAttribute(ParameterSetName="FromMember")]
+        public string[] UsingNamespace { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            if (AssemblyName != null)
+            {
+                AddAssemblies();
+            }
+        }
+
+        private void AddAssemblies()
+        {
+            foreach (string name in AssemblyName)
+            {
+                Assembly.Load(name);
+            }
+        }
+    }
+}
---
+++ Source/ReferenceTests/AddTypeCommandTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) Pash Contributors. License: GPL/BSD. See https://github.com/Pash-Project/Pash/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ReferenceTests
+{
+    [TestFixture]
+    public class AddTypeCommandTests : ReferenceTestBase
+    {
+        [Test]
+        public void AddTypeAddsAssemblyToCurrentAppDomain()
+        {
+            string result = ReferenceHost.Execute(
+@"Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+[System.AppDomain]::CurrentDomain.GetAssemblies() | Foreach-Object { $_.fullname.split(',')[0] }
+");
+
+            StringAssert.Contains("Microsoft.Build" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void TypesFromAddedAssemblyAvailable()
+        {
+            string result = ReferenceHost.Execute(
+@"Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+[Microsoft.Build.Evaluation.Project].FullName
+");
+
+            StringAssert.Contains("Microsoft.Build.Evaluation.Project" + Environment.NewLine, result);
+        }
+    }
+}
