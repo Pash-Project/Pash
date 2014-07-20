4ae709114dd873fd11805799f3c07b654bcce452
Matt Ward <ward.matt@gmail.com>
2014-07-20 10:34:13 +0100

    TEST: Fix Add-Type tests on Mono 2.10.8.1 Travis CI.
    On Travis we get an error running two tests:
    
    Test Error : ReferenceTests.AddTypeCommandTests.AddTypeAddsAssemblyToCurrentAppDomain
    Test Error : ReferenceTests.AddTypeCommandTests.TypesFromAddedAssemblyAvailable
    
    System.IO.FileNotFoundException : Could not load file or assembly
    'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
    or one of its dependencies. The system cannot find the file specified.
    at System.AppDomain.Load
    
    Switch to using System.Web instead of Microsoft.Build in the test.

--- Source/ReferenceTests/AddTypeCommandTests.cs
+++ Source/ReferenceTests/AddTypeCommandTests.cs
@@ -35,22 +35,22 @@ namespace ReferenceTests
         public void AddTypeAddsAssemblyToCurrentAppDomain()
         {
             string result = ReferenceHost.Execute(
-@"Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+@"Add-Type -AssemblyName 'System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
 [System.AppDomain]::CurrentDomain.GetAssemblies() | Foreach-Object { $_.fullname.split(',')[0] }
 ");
 
-            StringAssert.Contains("Microsoft.Build" + Environment.NewLine, result);
+            StringAssert.Contains("System.Web" + Environment.NewLine, result);
         }
 
         [Test]
         public void TypesFromAddedAssemblyAvailable()
         {
             string result = ReferenceHost.Execute(
-@"Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
-[Microsoft.Build.Evaluation.Project].FullName
+@"Add-Type -AssemblyName 'System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+[System.Web.HttpApplication].FullName
 ");
 
-            StringAssert.Contains("Microsoft.Build.Evaluation.Project" + Environment.NewLine, result);
+            StringAssert.Contains("System.Web.HttpApplication" + Environment.NewLine, result);
         }
 
         [Test]
