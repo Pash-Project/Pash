aa3fc4c5e21075ded7a557255fcc655f38b622f8
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-11-27 13:37:31 +0100

    Added tests for prefix increment/decrement. Also some for postfix

--- Source/ReferenceTests/Language/Operators/PostfixIncrementDecrementTests_7_1_5.cs
+++ Source/ReferenceTests/Language/Operators/PostfixIncrementDecrementTests_7_1_5.cs
@@ -7,19 +7,35 @@ namespace ReferenceTests.Operators
     public class PostfixIncrementDecrementOperatorTests_7_1_5 : ReferenceTestBase
     {
         [TestCase("$i = 0; $j = $i++; $j; $i", 0, 1)] // increments properly by 1, after assignment
+        [TestCase("$i = 0; ($i++); $i", 0, 1)] // parenthesis cause writing sideffects to pipeline
         [TestCase("$i = $null; $j = $i++; $j; $i", null, 1)] // with $null value
         [TestCase("$j = $i++; $j; $i", null, 1)] // not defined before
+        [TestCase("$i = 0.1; $j = $i++; $j; $i", 0.1d, 1.1d)] // double
         public void PostfixIncrement(string cmd, object assigned, object incremented)
         {
             ExecuteAndCompareTypedResult(cmd, assigned, incremented);
         }
 
+        [Test]
+        public void PostfixIncrementDecimal()
+        {
+            ExecuteAndCompareTypedResult("$i = 0.1D; $j = $i++; $j; $i", 0.1m, 1.1m);
+        }
+
         [TestCase("$i = 0; $j = $i--; $j; $i", 0, -1)] // increments properly by 1, after assignment
+        [TestCase("$i = 0; ($i--); $i", 0, -1)] // parenthesis cause writing sideffects to pipeline
         [TestCase("$i = $null; $j = $i--; $j; $i", null, -1)] // with $null value
         [TestCase("$j = $i--; $j; $i", null, -1)] // not defined before
+        [TestCase("$i = 0.1; $j = $i--; $j; $i", 0.1d, -0.9d)] // double
         public void PostfixDecrement(string cmd, object assigned, object incremented)
         {
             ExecuteAndCompareTypedResult(cmd, assigned, incremented);
+        }
+
+        [Test]
+        public void PostfixDerementDecimal()
+        {
+            ExecuteAndCompareTypedResult("$i = 0.1D; $j = $i--; $j; $i", 0.1m, -0.9m);
         }
 
         [Test]
@@ -51,13 +67,13 @@ namespace ReferenceTests.Operators
         }
 
         [Test]
-        public void IncrementDecrement_Spec_Example1()
+        public void PostfixIncrementDecrement_Spec_Example1()
         {
             ExecuteAndCompareTypedResult("$i = 0; $i++; $j = $i--; $j; $i;", 1, 0);
         }
 
         [Test]
-        public void IncrementDecrement_Spec_Example2()
+        public void PostfixIncrementDecrement_Spec_Example2()
         {
             var cmd = NewlineJoin(
                 "$a = 1,2,3",
@@ -70,7 +86,7 @@ namespace ReferenceTests.Operators
         }
 
         [Test]
-        public void IncrementDecrementInSameLine()
+        public void PostfixIncrementDecrementInSameLine()
         {
             var cmd = NewlineJoin(
                 "$a = 1,2,3",
