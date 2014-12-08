9327dd0502e39a0d88b661e9e37ad8d28e75d514
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-12-08 12:48:33 +0100

    Added tests for Export-ModuleMember

--- Source/ReferenceTests/Commands/ExportModuleMemberTests.cs
+++ Source/ReferenceTests/Commands/ExportModuleMemberTests.cs
@@ -1,17 +1,132 @@
 using System;
 using NUnit.Framework;
+using System.Management.Automation;
 
 namespace ReferenceTests
 {
     [TestFixture]
     public class ExportModuleMemberTests : ReferenceTestBase
     {
-        // TODO: export member by pattern
-        // TODO: if one export is defined, default (all functions) is overwritten
-        // TODO: call cmdlet without args: export nothing (not even functions as default)
-        // TODO: export multiple alias,functions,variables...
-        // TODO: export cmdlet not within a module throws InvalidOperationException, PermissionDenied, Modules_CanOnlyExecuteExportModuleMemberInsideAModule
+        private string _testModule = NewlineJoin(
+            "function foo { 'foo' };",
+            "function foobar { 'foobar' };",
+            "function bar { 'bar' };",
+            "$x = 1",
+            "$y = 2",
+            "$xy = 12",
+            "New-Alias lorem bar",
+            "New-Alias loremipsum bar",
+            "New-Alias ipsum bar"
+        );
 
+        [Test]
+        public void ExportModuleMemberByPattern()
+        {
+            var module = CreateFile(_testModule + "Export-ModuleMember -Function Foo* -Variable X* -Alias loRem*", "psm1");
+            var cmd = NewlineJoin(
+                "Import-Module '" + module + "';",
+                "foo; foobar;",
+                "$x; $xy",
+                "lorem; loremipsum;",
+                "$y" // shouldn't be exported
+                );
+            ExecuteAndCompareTypedResult(cmd, "foo", "foobar", 1, 12, "bar", "bar", null);
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("bar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("ipsum");
+            });
+        }
+
+
+        [Test]
+        public void ExportModuleMemberByArray()
+        {
+            var module = CreateFile(_testModule + "Export-ModuleMember -Function Foo,Bar -Variable X,Y -Alias loRem,Ipsum", "psm1");
+            var cmd = NewlineJoin(
+                "Import-Module '" + module + "';",
+                "foo; bar;",
+                "$x; $y",
+                "lorem; ipsum;",
+                "$xy" // shouldn't be exported
+                );
+            ExecuteAndCompareTypedResult(cmd, "foo", "bar", 1, 2, "bar", "bar", null);
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("foobar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("loremipsum");
+            });
+        }
+
+        [Test]
+        public void ExportModuleMemberVariablePreventsExportingFunctions()
+        {
+            var module = CreateFile(_testModule + "Export-ModuleMember -Variable x", "psm1");
+            var cmd = NewlineJoin(
+                "Import-Module '" + module + "';",
+                "$x; $y; $xy"
+                );
+            ExecuteAndCompareTypedResult(cmd, 1, null, null);
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("foobar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("foo");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("bar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("loremipsum");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("lorem");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("ipsum");
+            });
+        }
+
+        [Test]
+        public void ExportModuleMemberNothingPreventsExportingFunctions()
+        {
+            var module = CreateFile(_testModule, "psm1");
+            var cmd = NewlineJoin(
+                "Import-Module '" + module + "';",
+                "$x; $y; $xy"
+                );
+            ExecuteAndCompareTypedResult(cmd, null, null, null);
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("foobar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("foo");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("bar");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("loremipsum");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("lorem");
+            });
+            Assert.Throws<CommandNotFoundException>(delegate {
+                ReferenceHost.RawExecuteInLastRunspace("ipsum");
+            });
+        }
+
+        [Test]
+        public void ExportModuleMemberOutsideModuleThrows()
+        {
+            Assert.Throws<InvalidOperationException>(delegate {
+                ReferenceHost.Execute("Export-ModuleMember");
+            });
+        }
+
+        // TODO: test a module with cmdlets to export
     }
 }
 
---
+++ Source/System.Management/Microsoft.PowerShell/Commands/ExportModuleMemberCommand.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Microsoft.PowerShell.Commands
+{
+    public class ExportModuleMemberCommand
+    {
+        // TODO: export cmdlet not within a module throws InvalidOperationException, PermissionDenied, Modules_CanOnlyExecuteExportModuleMemberInsideAModule
+        public ExportModuleMemberCommand()
+        {
+        }
+    }
+}
+
