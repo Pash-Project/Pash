f0440b6b19cff4941a3309a3bb8706a98d483797
Matt Ward <ward.matt@gmail.com>
2015-02-09 21:44:59 +0000

    Support -Leaf for Split-Path

--- Source/Microsoft.PowerShell.Commands.Management/SplitPathCommand.cs
+++ Source/Microsoft.PowerShell.Commands.Management/SplitPathCommand.cs
@@ -9,26 +9,71 @@ using System.Management.Automation;
 
 namespace Microsoft.PowerShell.Commands
 {
-    [Cmdlet("Split", "Path")]
+    [Cmdlet(VerbsCommon.Split, "Path", DefaultParameterSetName = "ParentSet")]
     public class SplitPathCommand : CoreCommandWithCredentialsBase
     {
-        protected override void ProcessRecord()
-        {
-            foreach (Path path in Path)
-            {
-                WriteObject(path.GetParentPath(string.Empty).ToString());
-            }
-        }
-
         [Parameter(
             Position = 0,
-            Mandatory = true,
+            ParameterSetName = "ParentSet",
             ValueFromPipelineByPropertyName = true,
-            ValueFromPipeline = true),
-        Alias(new string[] { "PSPath" })]
+            ValueFromPipeline = true)]
+        [Parameter(
+            Position = 0,
+            ParameterSetName = "QualifierSet",
+            ValueFromPipeline = true)]
+        [Parameter(
+            Position = 0,
+            ParameterSetName = "LeafSet",
+            ValueFromPipeline = true)]
         public string[] Path { get; set; }
 
         [Parameter]
         public SwitchParameter Resolve { get; set; }
+
+        [Parameter(ParameterSetName = "IsAbsoluteSet")]
+        public SwitchParameter IsAbsolute { get; set; }
+
+        [Parameter(
+            ParameterSetName = "LeafSet",
+            ValueFromPipelineByPropertyName = true)]
+        public SwitchParameter Leaf { get; set; }
+
+        [ParameterAttribute(ParameterSetName = "LiteralPathSet")]
+        [Alias("PSPath")]
+        public string[] LiteralPath { get; set; }
+
+        [ParameterAttribute(ParameterSetName = "NoQualifierSet")]
+        public SwitchParameter NoQualifier { get; set; }
+
+        [ParameterAttribute(ParameterSetName = "ParentSet")]
+        public SwitchParameter Parent { get; set; }
+
+        [ParameterAttribute(Position = 1, ParameterSetName = "QualifierSet")]
+        public SwitchParameter Qualifier { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            foreach (Path path in Path)
+            {
+                ProcessPath(path);
+            }
+        }
+
+        private void ProcessPath(Path path)
+        {
+            if (Leaf.IsPresent)
+            {
+                WritePath(path.GetChildNameOrSelfIfNoChild().RemoveDrive());
+            }
+            else
+            {
+                WritePath(path.GetParentPath(string.Empty));
+            }
+        }
+
+        private void WritePath(Path path)
+        {
+            WriteObject(path.ToString());
+        }
     }
 }
--- Source/ReferenceTests/Commands/SplitPathTests.cs
+++ Source/ReferenceTests/Commands/SplitPathTests.cs
@@ -17,11 +17,79 @@ namespace ReferenceTests.Commands
         }
 
         [Test]
+        public void OneParentFolderOutputIsStringType()
+        {
+            string result = ReferenceHost.Execute(string.Format("(Split-Path 'parent{0}child').GetType().Name", Path.DirectorySeparatorChar));
+
+            Assert.AreEqual("String" + Environment.NewLine, result);
+        }
+
+        [Test]
         public void TwoParentFolders()
         {
             string result = ReferenceHost.Execute(string.Format("Split-Path parent1{0}child,parent2{0}child", Path.DirectorySeparatorChar));
 
             Assert.AreEqual(string.Format("parent1{0}parent2{0}", Environment.NewLine), result);
+        }
+
+        [Test]
+        public void Leaf()
+        {
+            string command = string.Format("Split-Path -Leaf parent{0}child.txt", Path.DirectorySeparatorChar);
+            string result = ReferenceHost.Execute(command);
+
+            Assert.AreEqual("child.txt" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void LeafOutputIsStringType()
+        {
+            string command = string.Format("(Split-Path -Leaf parent{0}child.txt).GetType().Name", Path.DirectorySeparatorChar);
+            string result = ReferenceHost.Execute(command);
+
+            Assert.AreEqual("String" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void LeafTwoItems()
+        {
+            string command = string.Format("Split-Path -Leaf parent1{0}child1.txt,parent2{0}child2.txt", Path.DirectorySeparatorChar);
+            string result = ReferenceHost.Execute(command);
+
+            Assert.AreEqual(NewlineJoin("child1.txt", "child2.txt"), result);
+        }
+
+        [Test]
+        public void LeafWithNoChild()
+        {
+            string result = ReferenceHost.Execute("Split-Path -Leaf parent");
+
+            Assert.AreEqual("parent" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void LeafOfEnvironmentDriveWithOneChild()
+        {
+            string result = ReferenceHost.Execute("Split-Path -Leaf env:foo");
+
+            Assert.AreEqual("foo" + Environment.NewLine, result);
+        }
+
+        [Test]
+        public void LeafOfEnvironmentDriveWithNoChild()
+        {
+            string result = ReferenceHost.Execute("Split-Path -Leaf env:");
+
+            Assert.AreEqual(Environment.NewLine, result);
+        }
+
+        [Test]
+        public void LeafOfEnvironmentDriveWithDirectorySeparatorAndOneChild()
+        {
+            string command = string.Format("Split-Path -Leaf env:{0}foo", Path.DirectorySeparatorChar);
+            string result = ReferenceHost.Execute(command);
+
+            Assert.AreEqual("foo" + Environment.NewLine, result);
         }
     }
 }
