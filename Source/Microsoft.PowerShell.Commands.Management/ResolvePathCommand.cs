0814b64e30da6150be00bc9a31e32c128847aee1
Stefan Burnicki <stefan.burnicki@burnicki.net>
2015-03-10 10:51:26 +0100

    Stub for ResolvePath; Changed CoreCommand class hierarchy a little so it can use Path parameters

---
+++ Source/Microsoft.PowerShell.Commands.Management/ResolvePathCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Microsoft.PowerShell.Commands.Management
+{
+    [Cmdlet("Resolve", "Path", DefaultParameterSetName="Path"
+        /*, SupportsTransactions=true, HelpUri="http://go.microsoft.com/fwlink/?LinkID=113384"*/)]
+    public class ResolvePathCommand : CoreCommandWithPathsBase
+    {
+        [Parameter]
+        public SwitchParameter Relative { get; set; }
+
+        protected override void ProcessRecord()
+        {
+
+        }
+    }
+}
+
