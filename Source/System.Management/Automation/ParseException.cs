ca2d02dad477204b7a9c856fc6e06493cfd02f8e
Stefan Burnicki <stefan.burnicki@burnicki.net>
2015-04-27 09:26:52 +0200

    Nicer error message showing the error location in src

--- Source/System.Management/Automation/ParseException.cs
+++ Source/System.Management/Automation/ParseException.cs
@@ -12,6 +12,7 @@ namespace System.Management.Automation
     [Serializable]
     public class ParseException : RuntimeException
     {
+        private const int _maxLineLength = 70;
         public string RawMessage { get; private set; }
         public string FormattedMessage { get; private set; }
 
@@ -27,16 +28,16 @@ namespace System.Management.Automation
         {
         }
 
-        public ParseException(string rawMessage) : this(rawMessage, -1, -1)
+        public ParseException(string rawMessage) : this(rawMessage, -1, -1, "")
         {
         }
 
-        public ParseException(string rawMessage, int line, int col)
+        public ParseException(string rawMessage, int line, int col, string srcText)
         {
             Id = "Parse";
             Category = ErrorCategory.ParserError;
             RawMessage = rawMessage;
-            FormattedMessage = FormatMessage(rawMessage, line, col);
+            FormattedMessage = FormatMessage(rawMessage, line, col, srcText);
         }
 
         public ParseException(string message, Exception innerException)
@@ -51,7 +52,7 @@ namespace System.Management.Automation
         {
         }
 
-        private string FormatMessage(string rawMessage, int line, int col)
+        private string FormatMessage(string rawMessage, int line, int col, string srcText)
         {
             var msg = new StringBuilder("Parse error");
             if (line >= 0 || col >= 0)
@@ -59,16 +60,46 @@ namespace System.Management.Automation
                 msg.AppendFormat(" at ({0}:{1})", line, col);
             }
             msg.Append(": ");
-            if (rawMessage.Length > 100)
+            if (rawMessage.Length > _maxLineLength)
             {
-                msg.Append(rawMessage.Substring(0, 97));
+                msg.Append(rawMessage.Substring(0, _maxLineLength - 3));
                 msg.Append("...");
             }
             else
             {
                 msg.Append(rawMessage);
             }
+            AddErrorLocationDescription(line, col, srcText, msg);
             return msg.ToString();
+        }
+
+        private void AddErrorLocationDescription(int line, int col, string srcText, StringBuilder msgBuilder)
+        {
+            const int leftFromError = 50;
+            if (line < 0 || col < 0)
+            {
+                return;
+            }
+            var srcLines = srcText.Split(new [] { "\r\n", "\n" }, StringSplitOptions.None);
+            if (line > srcLines.Length)
+            {
+                return;
+            }
+            var affectedLine = srcLines[line];
+            msgBuilder.AppendLine();
+            var cutLen = col - leftFromError;
+            if (cutLen > 0)
+            {
+                affectedLine = "..." + affectedLine.Substring(cutLen + 3);
+                col -= leftFromError;
+            }
+            if (affectedLine.Length > _maxLineLength)
+            {
+                affectedLine = affectedLine.Substring(0, _maxLineLength - 3) + "...";
+            }
+            msgBuilder.Append("> ");
+            msgBuilder.AppendLine(affectedLine);
+            msgBuilder.Append("^".PadLeft(col + 3)); // +1 because index offset, + 2 because '> ' in line above
         }
     }
 }
