317cdf700e4c5758b76eb405475648eb5cd954cc
Stefan Burnicki <stefan.burnicki@burnicki.net>
2016-03-04 00:52:59 +0100

    Test crashing with unsupported finally clause

---
+++ Source/ReferenceTests/Language/TryCatchFinallyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ReferenceTests.Language
+{
+    [TestFixture]
+    public class TryCatchFinallyTests : ReferenceTestBase
+    {
+        [Test]
+        public void TryFinallyWithoutCatch()
+        {
+            ExecuteAndCompareTypedResult(
+                "try { 2 + 2 } finally { 0 }",
+                4,
+                0
+            );                
+        }
+    }
+}
+
